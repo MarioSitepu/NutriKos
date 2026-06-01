63ac88e65ac1f1cdb03019f0bbb52123314ad9b2
MarioSitepu <stinart123@gmail.com>
2026-06-01 18:27:55 +0700

    fix: move authOptions out of route.ts to fix next build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,32 +1,5 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "../../../../lib/prisma";
-
-import type { NextAuthOptions } from "next-auth";
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async session({ session, user }) {
-      if (session?.user && user?.id) {
-        // user is provided when using database sessions
-        session.user = {
-          ...session.user,
-          id: user.id
-        } as any;
-      }
-      return session;
-    },
-  },
-};
+import { authOptions } from "../../../../lib/auth";
 
 const handler = NextAuth(authOptions);
 
